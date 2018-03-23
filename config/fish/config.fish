2e9287ed277d2b79446c29081597b0d727f4fbe0
Matthias Rampke <matthias@rampke.de>
2018-03-23 01:25:55 +0100

    Make fish config more defensive
    don't yell if there is no RVM or docker-machine
    
    Signed-off-by: Matthias Rampke <matthias@rampke.de>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,9 +2,14 @@ fish_hybrid_key_bindings
 set CDPATH . ~/src/github.com/
 set fish_greeting ""
 
-rvm default
+if test -f ~/.rvm/script/rvm
+    rvm default
+end
 
 set -x GOPATH $HOME
 set -x PATH ~/bin $PATH
 set -x EDITOR vim
-eval (docker-machine env default --shell fish)
+
+if command -v docker-machine > /dev/null
+    eval (docker-machine env default --shell fish)
+end
