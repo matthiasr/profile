362ceebcd4f857143f4c885f5d43d075d99a32ad
Matthias Rampke <matthias@rampke.de>
2020-05-19 07:47:40 +0000

    Add pyenv, reformat config.fish
    Signed-off-by: Matthias Rampke <matthias@rampke.de>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,21 +14,28 @@ end
 
 ssh-add -L >/dev/null 2>/dev/null
 if [ $status -eq 2 ]
-  pkill -u (whoami) ssh-agent
-  eval (ssh-agent -c)
+    pkill -u (whoami) ssh-agent
+    eval (ssh-agent -c)
 end
 
 set -x EDITOR vim
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/.iterm2_shell_integration.fish
+and source {$HOME}/.iterm2_shell_integration.fish
 
 # https://github.com/aws/aws-cli/issues/1079#issuecomment-252947755
-test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+test -x (which aws_completer)
+and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
 # https://github.com/fish-shell/fish-shell/issues/825#issuecomment-203021964
 function save_history --on-event fish_preexec
     history --save
 end
 
-# rbenv
-status --is-interactive; and type -q rbenv; and source (rbenv init -|psub)
+# rbenv & pyenv
+status --is-interactive
+and type -q rbenv
+and source (rbenv init -|psub)
+status --is-interactive
+and type -q pyenv
+and source (pyenv init -|psub)
