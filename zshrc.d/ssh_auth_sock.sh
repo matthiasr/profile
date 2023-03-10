c75490808cb9504404d0a7ea4a21dcd14119cdfe
Matthias Rampke <matthias@rampke.de>
2023-03-10 08:49:12 +0000

    set SSH_AUTH_SOCK
    for 1Password
    
    Signed-off-by: Matthias Rampke <matthias@rampke.de>

---
+++ zshrc.d/ssh_auth_sock.sh
@@ -0,0 +1,3 @@
+if [ -S "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" ]; then
+  export SSH_AUTH_SOCK="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+fi
