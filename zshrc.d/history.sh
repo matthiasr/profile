eda8bbdb8de7edfa824fd62b5d0b1de73dac9cd6
Matthias Rampke <matthias@rampke.de>
2023-07-04 08:06:46 +0000

    unsetopt sharehistory
    keep context within each terminal

---
+++ zshrc.d/history.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# I use multiple terminals for parallel streams of commands / contexts, let me go back in each stream.
+# Use fc -R to load history into the current shell.
+unsetopt sharehistory
